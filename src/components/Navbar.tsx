5cbdb47d8c023a686a8a2b7f6ecbd9c9506884ee
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-07-01 15:19:39 +0530

    Improved popout menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,8 +18,8 @@ const UserIcon = (props: { isUserMenuShowing: boolean, setIsUserMenuShowing: (me
             <div className="bg-white flex items-end justify-center w-8 h-8 md:w-10 md:h-10 rounded-full overflow-clip">
                 <img className="w-7 h-7 md:w-9 md:h-9 pointer-events-none" src={user} alt="user" />
             </div>
-            <div className={`${isUserMenuShowing ? "": "hidden"} rounded-xl p-5 md:px-8 md:py-10 bg-[#22667f] absolute top-11 flex flex-col justify-center items-start gap-2 md:gap-4 md:text-lg text-[#FFF1DC]`}>
-                <div className="" onClick={() => navigate("/profile")}>Profile</div>
+            <div className={`${isUserMenuShowing ? "": "hidden"} rounded-xl p-5 md:px-8 md:py-10 bg-[#22667f] absolute top-11 flex flex-col justify-center gap-2 md:gap-4 md:text-lg text-[#FFF1DC]`}>
+                <div className="w-full" onClick={() => navigate("/profile")}>Profile</div>
                 <div className="whitespace-nowrap" onClick={() => navigate("/change-password")}>Change Password</div>
                 <div className="" onClick={handleSignOut}>Signout</div>
             </div>
@@ -64,9 +64,9 @@ const Navbar = () => {
             </div>
             <div className="md:hidden flex justify-start items-center w-full fixed py-5 pl-5 bg-[#3A98B9]">
                 <img src={isMenuShowing ? close : menu} alt="menu icon" className="w-5 h-5 -scale-x-100" onClick={() => setIsMenuShowing(!isMenuShowing)}/>
-                <div className={`${isMenuShowing ? "flex" : "hidden"} rounded-xl p-5 bg-[#22667f] absolute top-11 flex flex-col justify-center items-start gap-2 text-[#FFF1DC] w-2/3`}>
+                <div className={`${isMenuShowing ? "flex" : "hidden"} rounded-xl p-5 bg-[#22667f] absolute top-11 flex flex-col justify-center gap-2 text-[#FFF1DC]`}>
                     {navLinks.map((navLink, index) => (
-                        <div key={index} onClick={() => handleLinkChange(navLink)} className={`${currPortal === navLink ? "font-semibold" : ""} text-[#FFF1DC] whitespace-nowrap hover:cursor-pointer w-1/3 text-center`}>{navLink !== "Anonymous Complaints/Suggestions" ? navLink : "Anonymous Complaints"}</div>
+                        <div key={index} onClick={() => handleLinkChange(navLink)} className={`${currPortal === navLink ? "font-semibold" : ""} w-full text-[#FFF1DC] whitespace-nowrap hover:cursor-pointer`}>{navLink !== "Anonymous Complaints/Suggestions" ? navLink : "Anonymous Complaints"}</div>
                     ))}
                 </div>
                 <UserIcon isUserMenuShowing={isUserMenuShowing} setIsUserMenuShowing={setIsUserMenuShowing} />
