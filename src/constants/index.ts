b2de49ee8b888aaf5108aeb3242407ae55075a07
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-07-05 15:29:54 +0530

    Tags are fetched from the server now

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,7 @@
-export const tags = ["Placement Cell", "Exam Cell", "Admission Office", "Student Cell"];
+fetch("http://localhost:3000/tags")
+    .then(response => response.json())
+    .then(data => {tags = data;})
+    .catch(error => console.log(error))
+
+
+export let tags = ["Placement Cell", "Exam Cell", "Admission Office", "Student Cell"];
